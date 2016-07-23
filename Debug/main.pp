5dd856eafa39a8272ee5fffd8b634d5bc23896a3
Simon Jones <srj52@canterbury.ac.nz>
2016-07-23 16:08:22 +1200

    uploading freerots

---
+++ Debug/main.pp
@@ -0,0 +1,33 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../include/FreeRTOS.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stddef.h
+main.obj: ../include/projdefs.h
+main.obj: ../include/../FreeRTOSConfig.h
+main.obj: ../include/portable.h
+main.obj: ../include/../portable/portmacro.h
+main.obj: ../include/mpu_wrappers.h
+main.obj: ../include/task.h
+main.obj: ../include/list.h
+main.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_types.h
+main.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_memmap.h
+main.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_sysctl.h
+main.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/sysctl.h
+main.obj: ../demo_code/basic_io.h
+
+../main.c: 
+../include/FreeRTOS.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stddef.h: 
+../include/projdefs.h: 
+../include/../FreeRTOSConfig.h: 
+../include/portable.h: 
+../include/../portable/portmacro.h: 
+../include/mpu_wrappers.h: 
+../include/task.h: 
+../include/list.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_types.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_memmap.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_sysctl.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/sysctl.h: 
+../demo_code/basic_io.h: 
---
+++ Debug/rit128x96x4.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+rit128x96x4.obj: ../rit128x96x4.c
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_ssi.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_memmap.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_sysctl.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_types.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/debug.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/gpio.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/pin_map.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/ssi.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/sysctl.h
+rit128x96x4.obj: P:/My\ Documents/2015/courses/ENCE361/StellarisWare/boards/ek-lm3s1968/drivers/rit128x96x4.h
+
+../rit128x96x4.c: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_ssi.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_memmap.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_sysctl.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/inc/hw_types.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/debug.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/gpio.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/pin_map.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/ssi.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/driverlib/sysctl.h: 
+P:/My\ Documents/2015/courses/ENCE361/StellarisWare/boards/ek-lm3s1968/drivers/rit128x96x4.h: 
