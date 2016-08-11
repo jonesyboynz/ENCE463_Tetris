63121882ec3a89b710dd54fc7c828f7fc0da0c11
Simon Jones <srj52@uclive.ac.nz>
2016-08-11 13:14:40 +1200

    added some more comments

--- game_engine.h
+++ game_engine.h
@@ -70,23 +70,33 @@ typedef struct game_s {
 extern Game base_game;
 
 void xGameEngineTask(void* parameters);
+//game engine task. runs the game engine
 
 void reset_game(Game* game);
+//resets a game structure
 
 Tetrominoe* get_current_tetrominoe(Game* game);
+//gets the currenly active tetrominoe from the game
 
 void draw_current_tetrominoe(Game* game);
+//draws the currently active tetrominoe form a game
 
 void erase_current_tetrominoe(Game* game);
+//erases the currently active tetrominoe from a game
 
 int spawn_new_tetrominoe(Game* game);
+//attempts to place the next tetrominoe on the game grid. returns a TRUE/FALSE value to indicate success and failure
 
 void get_next_tetrominoe(Game* game);
+//shifts the next tetrominoe into the current tetrominoe and randomly selects a new next tetrominoe
 
 void initalise_game(Game* game);
+//initalizes a game structure
 
 void place_current_tetrominoe(Game* game);
+//places the currently active tetrominoe in the game grid array
 
 int can_drop_current_tetrominoe(Game* game);
+//indicates whether or not the currently active tetrominoe can drop down.
 
 #endif
